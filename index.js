688544904f0c5805ede00cfa509e43bb9715d6a4
Selim Elshabrawy <selimy6541@gmail.com>
2026-06-06 15:05:00 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const axios = require("axios");
+
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/dsb-ping-echo", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+(async () => {
+  await app.start();
+  console.log("Echo is running!");
+})();
+
+app.command("/dsb-help-echo", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Available Commands:
+
+/dsb-ping-echo - Check bot latency
+/dsb-catfact-echo - Get a cat fact
+/dsb-joke-echo - Crack a Joke`
+
+  });
+});
+
+app.command("/dsb-catfact-echo", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+app.command("/dsb-joke-echo", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+    await respond({
+      text:
+`${response.data.setup}
+
+${response.data.punchline}`
+    });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a joke." });
+  }
+});
